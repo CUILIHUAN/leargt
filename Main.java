6613fb16f073f7c7e47e900b2954e174fcbc38af
root <1738127840@qq.com>
2018-10-15 10:15:16 +0800

    切换到分支上

--- Main.java
+++ Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println(ssplit[i]);
             System.out.println("hello");
             System.out.println("dsjfjsdfiewfwe");
+            System.out.println("切换到分支上");
 
         }
     }
